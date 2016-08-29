14211cf59230fcc106dc71e94f1eaf289f65e58b
Federico Rossi <Federico Rossi>
2016-08-29 10:28:15 +0200

    Added Model class to model out database's tables. Refactoring

---
+++ Model.php
@@ -0,0 +1,85 @@
+<?php
+require_once __DIR__ . "/QuickDB.php";
+function indexByKey($array,$key) {
+	return array_search($key, array_keys($array));
+}
+
+class Model extends QuickDB {
+	
+	private $name;
+	private $fields;
+	private $keys;
+
+	function __construct($modelName) {
+		parent::__construct();
+		$query = "SHOW COLUMNS FROM $modelName";
+		$this->name = $modelName;
+		$this->keys = array();
+		$fields = $this->query($query)->toArray(1);
+		foreach ($fields as $value) {
+			$this->fields[$value["Field"]] = null;
+			if($value["Key"]) array_push($this->keys,$value["Field"]);
+		}
+		$this->reset();
+	}
+
+	function create($fields) {
+		$query_0 = "INSERT INTO $this->name(";
+		$query_1 = "VALUES (";
+		foreach ($fields as $key => $value) {
+			$query_0.="$key";
+			$query_1.="'$value'"; //need to escape
+			if(indexByKey($fields,$key) != count($fields) - 1) {
+				$query_0.= ",";
+				$query_1.= ",";
+			}
+		}
+		$query = $query_0 . ") " . $query_1 . ")";
+		return $this->query($query)->toJSON(1);
+	}
+
+	function read($keys) {
+		$query = "SELECT * FROM $this->name WHERE";
+		foreach ($this->keys as $index => $key) {
+			$query.= " $key=$keys[$index]"; //need to escape
+			if($index != count($this->keys) - 1) $query.= " AND";
+		}
+		return $this->query($query)->toJSON(1);
+	}
+
+	function update($keys,$fields) {
+		$query = "UPDATE $this->name SET";
+		foreach($fields as $key => $value) {
+			$query.= " $key='$value'";
+			if(indexByKey($fields,$key) != count($fields) - 1) {
+				$query.=',';
+			}
+		}
+		$query.= " WHERE";
+		foreach ($this->keys as $index => $key) {
+			$query.= " $key=$keys[$index]"; //need to escape
+			if($index != count($this->keys) - 1) $query.= " AND";
+		}
+		return $this->query($query)->toJSON(1);
+	}
+
+	function delete($keys) {
+		$query = "DELETE FROM $this->name WHERE";
+		foreach ($this->keys as $index => $key) {
+			$query.= " $key='$keys[$index]'"; //need to escape
+			if($index != count($this->keys) - 1) $query.= " AND";
+		}
+		return $this->query($query)->toJSON(1);
+	}
+
+	function where($fields) {
+		$query = "SELECT * FROM $this->name WHERE";
+		foreach($fields as $key => $value) {
+			$query.= " $key='$value'";
+			if(indexByKey($fields,$key) != count($fields) - 1) {
+				$query.=' AND';
+			}
+		}
+		return $this->query($query)->toJSON(1);
+	}
+}
--- QuickDB.php
+++ QuickDB.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . "/config.php";
-class quickDB {
+class QuickDB {
 	protected $connection;
 	protected $result;
 	protected $error;
